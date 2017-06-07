a9a4622b709d11b0313b1152fec3c97574d40754
Kaan Keskinsoy <kk104@hdm-stuttgart.de>
2017-06-07 12:16:01 +0200

    Create route/humidity for saving new humidity data.

--- server/routes/humidity/humidity.js
+++ server/routes/humidity/humidity.js
@@ -10,24 +10,23 @@ const express = require('express');
 const router = express.Router();
 
 var mongoose = require('mongoose');
-var AirPressureModel = mongoose.model('AirPressure');
+var HumidityModel = mongoose.model('Humidity');
 var PIModel = mongoose.model('PI');
 
 // Weather data is added every 1 or 5 minutes
 router.post('/', (req, res) => {
-    if (!req.body || !req.body.pi_ID || !req.body.datetime || !req.body.temp_C || !req.body.pressure_Pa || !req.body.altitude_m) {
+    if (!req.body || !req.body.pi_ID || !req.body.datetime || !req.body.temp_C || !req.body.humidity_pct) {
         return res.status(500);
     }
     // check if pi_ID is registered
     PIModel.findById({ _id: req.body.pi_ID }, (err, pi) => {
         if (err) return res.status(403);
-        AirPressureModel.create(
+        HumidityModel.create(
             {
                 pi_id: pi._id,
                 datetime: new Date(req.body.datetime),
                 temp_C: req.body.temp_C,
-                pressure_Pa: req.body.pressure_Pa,
-                altitude_m: req.body.altitude_m
+                humidity_pct: req.body.humidity_pct,
             },
             (err, data) => {
                 if (err) return res.status(500);
