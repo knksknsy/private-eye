366efe68d1ade80e61f412672bfd5a39f28cb129
Kaan Keskinsoy <kk104@hdm-stuttgart.de>
2017-06-14 22:32:24 +0200

    Remove render methods for error handling.

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -79,7 +79,7 @@ if (app.get('env') === 'development') {
 // Production error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
